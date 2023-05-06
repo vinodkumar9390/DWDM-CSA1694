1dc67ceca0bd56656ca083a42852606a73dafb39
vinodkumar9390 <114823478+vinodkumar9390@users.noreply.github.com>
2023-05-06 23:49:26 +0530

    Add files via upload

---
+++ pie chart.r
@@ -0,0 +1,5 @@
+a <- c(80,70,50,60,70,100)
+result <- (pie(a,main="piechart",labels =c( "student 1","student 2","student 3","student 4","student 5","student 6"),
+               col = c("red", "orange", "yellow", "blue", "green","black")))
+print(result)
+
