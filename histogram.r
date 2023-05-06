08fa067c74f77da5064f20e92149600057505f32
vinodkumar9390 <114823478+vinodkumar9390@users.noreply.github.com>
2023-05-06 23:43:58 +0530

    Add files via upload

---
+++ histogram.r
@@ -0,0 +1,11 @@
+temperatures <- c(20, 22, 25, 29, 23, 27, 28)
+
+result <- hist(temperatures, 
+               main = "Maximum Temperatures in a Week", 
+               xlab = "Degree Celsius",
+               ylab = "Day",
+               names.arg = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"),
+               col="green"
+)
+print(result)
+
